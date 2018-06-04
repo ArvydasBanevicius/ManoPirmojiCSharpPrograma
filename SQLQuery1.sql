8a28327490fff6a84d362a2ab5bcf14b8542a09c
Arvydas Banevičius <arvydas.banevicius@gmail.com>
2018-06-04 19:08:38 +0300

    uzklausos

---
+++ SQLQuery1.sql
@@ -0,0 +1,21 @@
+﻿  /*select ReadingTime, DeviceData, ParameterName, DimensionName from DataCollection  
+	inner join DeviceList on DataCollection.DeviceID = DeviceList.Id 
+	inner join Parameter on DataCollection.ParameterID = Parameter.Id 
+	inner join Dimension on DataCollection.DimensionID = Dimension.Id 
+  where (DeviceList.Id = 1) 
+  */
+  
+select [DeviceTime],[Energy],[Volume],[Flow],[Power],[T1],[T2],[dT],[OnTime],[Error] 
+from 
+( 
+  select ReadingTime, DeviceData, ParameterName, DimensionName from DataCollection  
+	inner join DeviceList on DataCollection.DeviceID = DeviceList.Id 
+	inner join Parameter on DataCollection.ParameterID = Parameter.Id 
+	inner join Dimension on DataCollection.DimensionID = Dimension.Id 
+  where (DeviceList.Id = 1) 
+) src 
+pivot 
+( 
+  max(DeviceData) 
+  for ParameterName in([DeviceTime],[Energy],[Volume],[Flow],[Power],[T1],[T2],[dT],[OnTime],[Error]) 
+) pvt
